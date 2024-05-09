da2614b42d52112ef3c5e0d249b1e92341ae5215
Y-HuiMing-Y <119154148+Y-HuiMing-Y@users.noreply.github.com>
2024-05-10 00:04:42 +0800

    Update train_caption.py

--- train_caption.py
+++ train_caption.py
@@ -66,8 +66,7 @@ def evaluate(model, data_loader, device, config):
     print_freq = 10
 
     result = []
-    for image, image_id in metric_logger.log_every(data_loader, print_freq, header): 
-        
+    for image, image_id in metric_logger.log_every(data_loader, print_freq, header):
         image = image.to(device)       
         
         captions = model.generate(image, sample=True, num_beams=config['num_beams'], max_length=config['max_length'], 
