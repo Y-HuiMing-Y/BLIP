350b03396e0c15ede0c56675674075b848f40312
Y-HuiMing-Y <2417150656@qq.com>
2024-07-07 13:05:56 +0800

    Merge remote-tracking branch 'origin/main'
    # Conflicts:
    #	train_caption.py

--- models/vit.py
+++ models/vit.py
@@ -222,8 +222,9 @@ class VisionTransformer(nn.Module):
     def forward(self, x, register_blk=-1):
         # B = x.shape[0]  # 提取x的0号位作为批量大小
         B, C, H, W = x.shape
-        print(x.shape)
-        # x = DEConv(dim=384)
+        print("--------", x.shape)
+        x = DEConv(x)
+        print("--------", x.shape)
         x = self.patch_embed(x)  # 调用patch_embed对象将输入x转为嵌入序列
         cls_tokens = self.cls_token.expand(B, -1, -1)  # stole cls_tokens impl from Phil Wang, thanks
         # 扩展cls令牌的第一个维度为B，-1表示保持原维度不变
